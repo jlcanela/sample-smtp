84fff12b2a7105bcc239618ebffa8388d6290048
jlcanela <jlcanelam@gmail.com>
2014-06-01 23:38:53 +0200

    make smtp sending more robust on smtp error

--- src/main/scala/org.ansoft.smtp/sender/SmtpSenderActor.scala
+++ src/main/scala/org.ansoft.smtp/sender/SmtpSenderActor.scala
@@ -6,23 +6,54 @@ import com.sun.mail.smtp.SMTPTransport
 import javax.mail.{Session, Message, URLName}
 import org.ansoft.smtp.reaper.Reaper
 import org.ansoft.smtp.reader.ReaderActor
+import scala.concurrent.duration._
 
 class SmtpSenderActor(session: Session) extends Actor with ActorLogging {
 
-  val transport = new SMTPTransport(session, new URLName("127.0.0.1:25"))
+  var transport : Option[SMTPTransport] = None
 
-  transport.connect()
+  def reconnectIfRequired {
+    if (transport.map(_.isConnected) getOrElse false) {
+      // we're connected so assuming it's ok
+    } else {
+      try {
+        val t = new SMTPTransport(session, new URLName("127.0.0.1:25"))
+        t.connect()
+        transport = Some(t)
+      } catch {
+        case ex: Exception =>
+          context.actorSelection("/user/logging") ! ("ERROR", ex.getMessage)
+      }
+
+    }
+  }
+
+  def resend(m:MimeMessage) {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    context.system.scheduler.scheduleOnce(5 seconds) {
+      self ! m
+    }
+  }
+
+  def sendMessage(m:MimeMessage) = try {
+    transport.map(t => {
+      t.sendMessage(m, m.getRecipients(Message.RecipientType.TO))
+      context.actorSelection("/user/logging") ! ("INFO", "sent email")
+      context.actorSelection("/user/reader") ! ReaderActor.Read
+    }) getOrElse {
+      resend(m)
+    }
+  } catch {
+    case ex: Exception =>
+      transport.map(_.close)
+      context.actorSelection("/user/logging") ! ("ERROR", ex.getMessage)
+      resend(m)
+  }
 
   def receive = {
     case m: MimeMessage ⇒
-      try {
-        transport.sendMessage(m, m.getRecipients(Message.RecipientType.TO))
-        context.actorSelection("/user/logging") ! ("INFO", "sent email")
-      } catch {
-        case ex: Exception => context.actorSelection("/user/logging") ! ("ERROR", ex.getMessage)
-      }
-
-      context.actorSelection("/user/reader") ! ReaderActor.Read
+      reconnectIfRequired
+      sendMessage(m)
   }
 
   override def preStart {
@@ -31,7 +62,7 @@ class SmtpSenderActor(session: Session) extends Actor with ActorLogging {
   }
 
   override def postStop {
-    transport.close
+    transport.map(_.close)
   }
 
 }
